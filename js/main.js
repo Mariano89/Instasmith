75f3a09acfe3f7cb8ce1f1067ab29ca9249ea3c9
Travis Sorensen <travis.sorensen@gmail.com>
2015-05-27 15:17:41 -0700

    save input

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 // window.onload=function(){
 
-	var form = document.getElementById('form');
-	var user = document.getElementById('user').value;
-	function myFunction(){
-		// var variable = user;
-		alert(user);
-	}
+	// var form = document.getElementById('form');
+	// var user = document.getElementById('user').value;
+	// function myFunction(){
+	// 	// var variable = user;
+	// 	alert(user);
+	//}
 // }
+$(document).ready(function(){
+	console.log("ready");
+	$(".login").on('click',function(e){
+		console.log("still ready");
+		var UserName = $('#user').val();
+		var PassWord = $('#pw').val();
+		console.log(UserName, PassWord);
+		e.preventDefault();
+	})
+	
+});
+	
