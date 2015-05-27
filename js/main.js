04058d699394357e3d46955237ce3a75717a942c
mariano89 <marianoe1989@gmail.com>
2015-05-27 14:13:37 -0700

    some styling

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// window.onload=function(){
+
+	var form = document.getElementById('form');
+	var user = document.getElementById('user').value;
+	function myFunction(){
+		// var variable = user;
+		alert(user);
+	}
+// }
