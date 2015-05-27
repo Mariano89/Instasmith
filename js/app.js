9df0facd5239650de337509e320f23ea986b92d1
mariano89 <marianoe1989@gmail.com>
2015-05-27 12:45:36 -0700

    outline of login page

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-// var http = require('http');
-// var fs = require('fs');
-
-// http.createServer(function(request, response){
-// 	response.writeHead(200, {
-// 		'Content-Type': 'text/html'});
-// 	fs.readFile('index.html', function(error, contents){
-// 		response.write(contents);
-// 		response.end();
-// 	});
-// }).listen(8080);
-
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
-});
-server.listen(8080);
+ http.createServer(function(request, response){
+	response.writeHead(200, {
+		'Content-Type': 'text/html'});
+	fs.readFile('../index.html', function(error, contents){
+		response.write(contents);
+		response.end();
+	});
+}).listen(8080);
+
+// var http = require('http');
+
+// var server = http.createServer(function(req, res) {
+//   res.writeHead(200);
+//   res.end('Hello Http');
+// });
+// server.listen(8080);
