d4b42b178e465e27b00504a49a33630f9826f768
mariano89 <marianoe1989@gmail.com>
2015-05-28 16:21:13 -0700

    added picture and cookie

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 var http = require('http');
 var fs = require('fs');
 
+
  http.createServer(function(request, response){
+ 	// var home = '../home.html';
+ 	// var mainStyle = '../main.css';
+ 	// var homeStyle = '../home.css';
+ 	
+	if(request.url == '/'){
 	response.writeHead(200, {
 		'Content-Type': 'text/html'});
 
-	// console.log(request.url)
+	console.log(request.url);
 	fs.readFile('../index.html', function(error, contents){
 		response.write(contents);
 		response.end();
 	});
+} else {
+	fs.readFile('../' + request.url, function(error, contents){
+		response.write(contents);
+		response.end();
+});
+}
 }).listen(8080);
 
 
--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
+	
 $(document).ready(function(){
 	var username;
 	var password;
-
-
+	//document.cookie = "this is saved";
+	var current_user = document.cookie; 
+	//console.log("cookie", current_user);
 
 	$(".login").on('click',function(e){
 		username = $('#user').val().toLowerCase();
-		console.log(username);
+		//console.log(username);
 		pw = $('#pw').val();
 		e.preventDefault();
+		
 		var userCheck = function(){
 			if(users.hasOwnProperty(username) && users[username]["password"] === pw){
-				// console.log("true");
+				document.cookie = username;
+				//console.log("document.cookie", document.cookie);
+				//users['current'] = users['current'][username];
+				//$('.hello').appendTo('Welcome ' + users[username]);
 				window.location.href = "home.html";
+				//alert(document.cookie);
+
 				// HomePics();
 			}
 			else {
@@ -23,6 +31,13 @@ $(document).ready(function(){
 		}();
 	});
 	
+	$('.test').on('click', function(){
+		console.log(document.cookie, "cookie");
+		console.log("current user", current_user);
+	});
+
+
+
 	var users = {
 		travis: {
 			password: 'travis123'
@@ -52,8 +67,10 @@ $(document).ready(function(){
 	  var items = [];
 	  $.each( data, function( key, val ) {
 	    items.push( "<img id='" + key + "' "+"src='"+val+"'>");
-	    items.push("<div class='likeBox'></div>");
-	  
+	    items.push("<div class='likeBox'><div class='likeIcon'></div></div>");
+	  	items.push("<div class='comment_display'></div>");
+	  	items.push("<input class='comment_input'>");
+	  	
 	  // console.log(items[key], items[val]);
 	  });
 	 $('<ul class="my-new-list"></ul>').append(items.join("")).appendTo('.home-screen-pics');
@@ -63,6 +80,50 @@ $(document).ready(function(){
 	  //   html: items.join("")
 	 	//  }).appendTo( ".home-screen-pics" );
 	});
+
+
+	var img = document.createElement('img');
+	function getPicture(user){
+		if(user == "alex"){
+			img.src = 'images/alex.jpeg';
+			document.getElementById('img_div').appendChild(img);
+		} else if (user == "david"){
+			img.src = 'images/david.jpeg';
+			document.getElementById('img_div').appendChild(img);
+		}
+		else if (user == "mariano"){
+			img.src = 'images/mariano.jpeg';
+			document.getElementById('img_div').appendChild(img);
+		}
+		else if (user == "travis"){
+			img.src = 'images/travis.jpeg';
+			document.getElementById('img_div').appendChild(img);
+		}
+		else if (user == "victoria"){
+			img.src = 'images/victoria.jpeg';
+			document.getElementById('img_div').appendChild(img);
+		}
+		else if (user == "will"){
+			img.src = 'images/will.jpeg';
+			document.getElementById('img_div').appendChild(img);
+		}
+	};
+
+	getPicture(current_user);
+	
+	var p = document.createElement('p');
+		function welcomeUser(user){
+			if(current_user){
+			p.innerHTML = "Welcome " +  current_user + "!";
+			document.getElementById('img_div').appendChild(p);
+		}
+		
+	}
+
+		welcomeUser(current_user);
+
+
+	$(".user").append(pictureArray).appendTo("<div class='home-screen-pics'></div>");
 
 
 
